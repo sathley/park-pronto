1d39b92e04f2ac435ffbae0dd6e8eb4883ce2c8c
Pratikbit <pratik.bhangale1992@gmail.com>
2016-09-29 03:08:26 -0400

    Adds top layout and xmls for parking zones

--- ParkPronto/app/src/main/java/edu/umbc/parkpronto/MapActivity.java
+++ ParkPronto/app/src/main/java/edu/umbc/parkpronto/MapActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,10 +22,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback ,View.OnClickListener {
 
     private GoogleMap mMap;
-
+    private Button mButtonA,mButtonB,mButtonC,mButtonD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +42,18 @@ public class MapActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        initializeUI();
+    }
+
+    private void initializeUI() {
+        mButtonA = (Button) findViewById(R.id.input1btn);
+        mButtonB = (Button) findViewById(R.id.input2btn);
+        mButtonC = (Button) findViewById(R.id.input3btn);
+        mButtonD = (Button) findViewById(R.id.input4btn);
+        mButtonA.setOnClickListener(this);
+        mButtonB.setOnClickListener(this);
+        mButtonC.setOnClickListener(this);
+        mButtonD.setOnClickListener(this);
     }
 
     @Override
@@ -86,5 +99,38 @@ public class MapActivity extends AppCompatActivity
         LatLng umbc = new LatLng(39.255858, -76.711184);
         mMap.addMarker(new MarkerOptions().position(umbc).title("Marker in UMBC"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(umbc));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.input1btn :
+                mButtonA.setBackground(getDrawable(R.drawable.roundbtna));
+                mButtonB.setBackground(getDrawable(R.drawable.roundbtn_unselected));
+                mButtonC.setBackground(getDrawable(R.drawable.roundbtn_unselected));
+                mButtonD.setBackground(getDrawable(R.drawable.roundbtn_unselected));
+                break;
+
+            case R.id.input2btn :
+                mButtonA.setBackground(getDrawable(R.drawable.roundbtn_unselected));
+                mButtonB.setBackground(getDrawable(R.drawable.roundbtnb));
+                mButtonC.setBackground(getDrawable(R.drawable.roundbtn_unselected));
+                mButtonD.setBackground(getDrawable(R.drawable.roundbtn_unselected));
+                break;
+            case R.id.input3btn :
+                mButtonA.setBackground(getDrawable(R.drawable.roundbtn_unselected));
+                mButtonB.setBackground(getDrawable(R.drawable.roundbtn_unselected));
+                mButtonC.setBackground(getDrawable(R.drawable.roundbtnc));
+                mButtonD.setBackground(getDrawable(R.drawable.roundbtn_unselected));
+                break;
+
+            case R.id.input4btn :
+                mButtonA.setBackground(getDrawable(R.drawable.roundbtn_unselected));
+                mButtonB.setBackground(getDrawable(R.drawable.roundbtn_unselected));
+                mButtonC.setBackground(getDrawable(R.drawable.roundbtn_unselected));
+                mButtonD.setBackground(getDrawable(R.drawable.roundbtnd));
+                break;
+        }
     }
 }
